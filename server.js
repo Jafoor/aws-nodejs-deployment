ea1b0208775dad73b0178136c89bd3873c6dbdce
Jafoor <abujafor.cse11@gmail.com>
2025-03-10 14:04:46 +0600

    add new request

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ app.get('/api/get', (req, res) => {
     res.send({
         name: 'Abu Jafor',
         date: new Date(),
-        ip: req.ip,
-        method: req.method,
         url: req.url
     })
 })
+
+app.get("/api/get/:id", (req, res) => {
+        res.send({
+            name: 'Abu Jafor',
+            date: new Date(),
+            id: req.params.id
+        })
+    })
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
